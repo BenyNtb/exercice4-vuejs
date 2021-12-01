41346935a2b5638ed56f6da1ad54ce6c42284b9d
Beny <b.ntambwe97@gmail.com>
2021-12-01 19:55:01 +0100

    components

--- src/App.vue
+++ src/App.vue
@@ -18,16 +18,16 @@
         img-src="https://i.pinimg.com/originals/f9/11/d3/f911d38579709636499618b6b3d9b6f6.jpg"
       >
       <img src="./assets/image-tanya.jpg" alt="tanya">
-      <h3>{{para1}}</h3>
-    <p>{{name1}}</p>
-    <p>{{job1}}</p></b-carousel-slide>
-
+      <Carousel para="I've been interested in coding for a whilr but never taken the jump, until now. I couldn't recommend this course enough. I'm now in the job of my dreams and so excited about the future.."/>
+      <Carousel name="Tanya Sinclair"/>
+      <Carousel job="UX Engineer"/>
+  </b-carousel-slide>
       <!-- Slides with custom text -->
       <b-carousel-slide img-src="https://i.pinimg.com/originals/f9/11/d3/f911d38579709636499618b6b3d9b6f6.jpg">
          <img src="./assets/image-john.jpg" alt="tanya">
-      <h3>{{para2}}</h3>
-    <p>{{name2}}</p>
-    <p>{{job2}}</p>
+      <Carousel name="If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer"/>
+      <Carousel name="John Tarkpor"/>
+      <Carousel job="Junior Front-end Developer"/>
       </b-carousel-slide>
 
     </b-carousel>
@@ -38,18 +38,15 @@
 </template>
 
 <script>
-
+  import Carousel from './components/Carousel.vue'
   export default {
+    components: {
+      Carousel
+    },
     data() {
       return {
         slide: 0,
         sliding: null,
-        para1: "I've been interested in coding for a whilr but never taken the jump, until now. I couldn't recommend this course enough. I'm now in the job of my dreams and so excited about the future..",
-        name1: "Tanya Sinclair",
-        job1: "UX Engineer",
-        para2: "If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer",
-        name2: "John Tarkpor",
-        job2: "Junior Front-end Developer",
       }
     },
     methods: {
---
+++ src/components/Carousel.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h3>{{para}}</h3>
+        <p>{{name}}</p>
+        <p>{{job}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Carousel',
+        props: {
+            para: String,
+            name: String,
+            job: String,
+        }
+    }
+</script>
